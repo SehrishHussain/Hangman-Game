0cd73172820e26d11d9a51e8db729fcedcd026d5
SehrishHussain <sehrishaslam1@hotmail.com>
2023-04-12 12:38:03 +0500

    logic improvements

--- hangman.py
+++ hangman.py
@@ -1,12 +1,17 @@
 import random
 import re
+import sys
+
 
 def main():
-    words = ['able', 'abo ut', 'acco unt']
+    words = ['able', 'abo ut', 'o unt']
     secret_word = random.choice(words)
-    print('Welcome to Hangman')
+    print('############################################################################', '\n'
+          '#######################Welcome to Hangman###################################', '\n'
+          '############################################################################')
 
     correct_guess = []
+    incorrect_guess = []
 
     def calculate_total_guess(secret_word):
         if len(secret_word) <= 7:
@@ -14,10 +19,9 @@ def main():
         else:
             return 8
 
-    def display_word(guess_word, correct_guess):
+    def display_word(guess_word, correct_guess, incorrect_guess):
 
         for input_letter in correct_guess:
-
             indicies = [i.start() for i in re.finditer(input_letter, secret_word)]
             for x in indicies:
                 guess_word = guess_word[:x] + input_letter + guess_word[x+1:]
@@ -27,10 +31,11 @@ def main():
             exit()
         else:
             print("\n""THE WORD IS:", guess_word)
+            print("Guessed letters are: ", correct_guess)
+            print("Incorrect guessed letters are: ", incorrect_guess)
 
+    def get_user_input(secret_word, total_guesses):
 
-    def get_user_input(secret_word, total_guesses, correct_guess):
-        # guess_word = secret_word.replace(secret_word, len(secret_word) * '*')
         guess_word = ""
         for letter in secret_word:
             if letter == " ":
@@ -40,27 +45,41 @@ def main():
         print("The secret word is:", guess_word)
 
         while total_guesses > 0:
+            print(f'You have total {total_guesses}  guesses!')
             input_letter = input("Guess the letter: ")
 
             if input_letter in secret_word:
                 if input_letter in correct_guess:
-                    print("Letter already guessed")
-                    print("Guessed letters are: ", correct_guess, '\n')
+                    print("Letter already guessed!")
                 else:
                     correct_guess.append(input_letter)
                     print("Correct guess!")
-                    print("Guessed letters are: ", correct_guess)
-                    display_word(guess_word, correct_guess)
-
+                    display_word(guess_word, correct_guess, incorrect_guess)
+            elif input_letter in incorrect_guess:
+                print("Incorrect letter already guessed!")
             elif input_letter not in secret_word:
                 total_guesses = total_guesses - 1
-                print(f'Incorrect guess. You have total {total_guesses} left')
-                display_word(guess_word, correct_guess)
-        print("You Lose!")
+                print("Incorrect guess.")
+                
+                if total_guesses == 0:
+                    print("You Lose!")
+                    print("The word was: ", secret_word, '\n')
+                else:
+                    incorrect_guess.append(input_letter)
+                    display_word(guess_word, correct_guess, incorrect_guess)
+
+
+
+        while True:
+            answer = input("Do you want to play again? Type YES or NO to exit: ")
+            if answer.lower() == "no":
+                sys.exit()
+            elif answer.lower() == "yes":
+                main()
     total_guesses = calculate_total_guess(secret_word)
     print('total_guesses: ', total_guesses)
 
-    get_user_input(secret_word, total_guesses, correct_guess)
+    get_user_input(secret_word, total_guesses)
 
 
 main()
