8689277da4bc715032ff4d47205912b372844323
SehrishHussain <sehrishaslam1@hotmail.com>
2023-04-12 21:28:16 +0500

    added list of phrases and words

--- hangman.py
+++ hangman.py
@@ -4,11 +4,35 @@ import sys
 
 
 def select_word():
-    words = ['able', 'abo ut', 'o unt']
-    secret_word = random.choice(words)
-    print('############################################################################', '\n'
-          '#######################Welcome to Hangman###################################', '\n'
-          '############################################################################')
+    words = ['cat', 'dog', 'book', 'sun', 'rain', 'tree', 'mountain', 'ocean', 'moon', 'star',
+             'beach', 'bird', 'cloud', 'coffee', 'computer', 'flower', 'friend', 'guitar', 'home', 'music',
+             'phone', 'pizza', 'river', 'smile', 'snow', 'song', 'water', 'wine', 'world',
+             'angel', 'apple', 'banana', 'butterfly', 'cake', 'camera', 'candle', 'dragon', 'elephant', 'fire',
+             'heart', 'horse', 'ice', 'key', 'light', 'love', 'magic', 'mirror', 'peace', 'sky']
+    phrases = ['love hurts sometimes more',
+               'cats purr dogs bark',
+               'rain falls plants grow',
+               'life is tough survive it',
+               'time flies memories last',
+               'dreams come true occasionally',
+               'money talks wealth whispers',
+               'silence is golden sometimes',
+               'mind over matter',
+               'hope springs eternal',
+               'actions speak louder',
+               'all for one one for all',
+               'less is more sometimes',
+               'what goes around comes',
+               'knowledge is power truly',
+               'beauty is in simplicity',
+               'truth is stranger than',
+               'curiosity killed the cat',
+               'live and learn hopefully',
+               'actions speak louder always']
+    combined_list = words + phrases
+    random.shuffle(combined_list)
+    secret_word = random.choice(combined_list)
+    print("SECRET WORD",secret_word)
     return secret_word
 
 
@@ -31,7 +55,7 @@ def display_word(guess_word, correct_guess, incorrect_guess, secret_word):
         print("Hurray! YOU WON!")
         want_to_play_again()
     else:
-        print("\n"f"THE WORD IS: {guess_word}")
+        print(f"\nTHE WORD IS: {guess_word}")
         print(f"Guessed letters are: {correct_guess}")
         print(f"Incorrect guessed letters are: {incorrect_guess}")
 
@@ -53,7 +77,6 @@ def get_user_input(secret_word, total_guesses, correct_guess, incorrect_guess):
             input_letter = input("Guess the letter: ")
             input_letter = input_letter.lower()
             if not input_letter.isalpha() or len(input_letter) != 1:
-                print("CHECK")
                 print("Invalid input. Please enter a single alphabet!")
             else:
                 break
@@ -75,7 +98,7 @@ def get_user_input(secret_word, total_guesses, correct_guess, incorrect_guess):
 
             if total_guesses == 0:
                 print("You Lose!")
-                print(f"The word was: {secret_word} " '\n')
+                print(f"The word was: {secret_word}")
             else:
                 incorrect_guess.append(input_letter)
                 display_word(guess_word, correct_guess, incorrect_guess, secret_word)
@@ -85,7 +108,7 @@ def get_user_input(secret_word, total_guesses, correct_guess, incorrect_guess):
 
 def want_to_play_again():
     while True:
-        answer = input("Do you want to play again? Type YES or NO to exit: ")
+        answer = input("\nDo you want to play again? Type YES or NO to exit: ")
         if answer.lower() == "no":
             sys.exit()
         elif answer.lower() == "yes":
@@ -93,6 +116,9 @@ def want_to_play_again():
 
 
 def main():
+    print('############################################################################', '\n'
+          '####################### Welcome to Hangman #################################', '\n'
+          '############################################################################')
     correct_guess = []
     incorrect_guess = []
     secret_word = select_word()
